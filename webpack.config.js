e25e7d45f7764ecd4a74dcf88abc064a2ec7fba0
Maria Secco <mapradosecco@gmail.com>
2024-03-01 19:11:25 -0300

    correcting merge problems

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
+const path = require('path');
+=======
+>>>>>>> c3734d001432938808cb8bf8cf512f4f6dbcd257
 
+<<<<<<< HEAD
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
+=======
 const path = require('path');
 
 module.exports = {
@@ -9,3 +22,5 @@ module.exports = {
   },
 }
 
+
+>>>>>>> c3734d001432938808cb8bf8cf512f4f6dbcd257
