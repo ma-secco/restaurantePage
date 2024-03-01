cd4416c6774b72f1550cbfe905580fa15164dbc1
Maria Secco <mapradosecco@gmail.com>
2024-03-01 19:11:47 -0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-=======
->>>>>>> c3734d001432938808cb8bf8cf512f4f6dbcd257
-
-<<<<<<< HEAD
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-}
-=======
 const path = require('path');
 
 module.exports = {
@@ -21,6 +7,3 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 }
-
-
->>>>>>> c3734d001432938808cb8bf8cf512f4f6dbcd257
