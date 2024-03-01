76d67805132eb8bad5e2b07e30bb5ffec597b112
Maria Beatriz P. Secco <mapradosecco@gmail.com>
2024-03-01 18:35:34 -0300

    complete space

---
+++ src/index.js
@@ -0,0 +1,6 @@
+alert("ta indo :)")
+
+// You might want to include an image, headline, and some text about how wonderful the restaurant is; 
+// you do not have to make this look too fancy. 
+// It’s okay to hard-code these into the HTML for now just to see how they look on the page.
+
