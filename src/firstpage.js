752f873bbaaa77c35b29528cb966306b8adeceb5
Maria Secco <mapradosecco@gmail.com>
2024-03-01 19:39:50 -0300

    .

--- src/firstpage.js
+++ src/firstpage.js
@@ -1,8 +1,23 @@
+const contentDiv = document.querySelector('#content')
+
 const functionPage = () => {
 
 }
 
+function about () {
 
+    const section = document.createElement('section');
+    section.setAttribute('id', 'sobre')
+    contentDiv.appendChild(section);
+
+    const titleAbout = document.createElement('h2');
+    titleAbout.innerText = "Sobre o restaurante"
+
+    const paragraphAbout = document.createElement('p');
+    paragraphAbout.innerText = "Descrição breve do restaurante"
+
+
+}
 
 
 
