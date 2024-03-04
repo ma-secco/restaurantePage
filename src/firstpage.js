907176e106df5faccd3d01383d1320069e2d90e9
Maria Secco <mapradosecco@gmail.com>
2024-03-04 15:40:57 -0300

    corrected code

--- src/firstpage.js
+++ src/firstpage.js
@@ -1,27 +1,21 @@
-const contentDiv = document.querySelector('#content')
+const contentDiv = document.querySelector('#content');
 
-const functionPage = () => {
-
-    const aboutSection = about()
-    return contentDiv.appendChild(aboutSection)
-}
-
-function about () {
-
+function about() {
     const section = document.createElement('section');
-    section.setAttribute('id', 'sobre')
+    section.setAttribute('id', 'sobre');
     contentDiv.appendChild(section);
 
     const titleAbout = document.createElement('h2');
-    titleAbout.innerText = "Sobre o restaurante"
+    titleAbout.innerText = "Sobre o restaurante";
+    section.appendChild(titleAbout);
 
     const paragraphAbout = document.createElement('p');
-    paragraphAbout.innerText = "Descrição breve do restaurante"
-
-
-
+    paragraphAbout.innerText = "Descrição breve do restaurante";
+    section.appendChild(paragraphAbout);
 }
 
+const functionPage = () => {
+    about();
+}
 
-
-export {functionPage }
+export { functionPage };
