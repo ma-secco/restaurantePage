356d00f23312e2e9283c75b387e875463aef00fc
Maria Secco <mapradosecco@gmail.com>
2024-03-04 16:05:33 -0300

    menu added

--- src/firstpage.js
+++ src/firstpage.js
@@ -1,5 +1,10 @@
 const contentDiv = document.querySelector('#content');
 
+const functionPage = () => {
+    about();
+    menu();
+}
+
 function about() {
     const section = document.createElement('section');
     section.setAttribute('id', 'sobre');
@@ -14,8 +19,33 @@ function about() {
     section.appendChild(paragraphAbout);
 }
 
-const functionPage = () => {
-    about();
-}
+function menu() {
+    const section = document.createElement('section');
+    section.setAttribute('id', 'menu');
+    contentDiv.appendChild(section);
 
+    const titleAbout = document.createElement('h2');
+    titleAbout.innerText = "Menu";
+    section.appendChild(titleAbout);
+
+    const paragraphAbout = (item, description, price ) => {
+        const div = document.createElement('div');
+        div.classList = "item-menu"
+
+        const dishTitle = document.createElement('h3');
+        dishTitle.innerText = item;
+
+        const dishDescription = document.createElement('p');
+        dishDescription.innerText = description;
+
+        const dishPrice = document.createElement('span');
+        dishPrice.innerText = price;
+
+        div.appendChild(dishTitle);
+        div.appendChild(dishDescription);
+        div.appendChild(dishPrice);
+
+        return div
+    }
+}
 export { functionPage };
